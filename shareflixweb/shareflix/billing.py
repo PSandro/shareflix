97a07fd5c8a63fc6339c439aa1b91ff58eb45e02
Sandro Pischinger <mail@sandropischinger.de>
2021-12-25 17:15:59 +0100

    keep it simple stupid

--- shareflixweb/shareflix/billing.py
+++ shareflixweb/shareflix/billing.py
@@ -1,4 +1,4 @@
-from shareflix.models import FinancialClaim, User, NetflixAccount, Currency
+from shareflix.models import FinancialClaim, User, NetflixAccount
 import datetime
 
 
@@ -39,40 +39,13 @@ def charge_account(
         beginning = ending
 
 
-    worth = subtract_amount_and_get_worth(account, amount)
 
-    worth_part = worth / account.user_set.all().count
+    amount_part = worth / account.user_set.all().count
 
     for user in account.user_set.all():
         charge_user(user,
                     beginning,
                     ending,
                     account,
-                    worth_part)
+                    amount_part)
 
-
-
-def substract_amount_and_get_worth(account: NetflixAccount, amount: float = 0., currency: Currency=Currency.TL):
-    available_cards = account.netflixgiftcard_set.filter(
-                                                        currency__exact=currency).filter(current_amount__gt=0.).order_by('-redemption_date')
-    rest_amount = amount
-    worth = 0.
-
-    for card in available_cards:
-        if (rest_amount > card.current_amount):
-            rest_amount -= card.current_amount
-            
-            worth += card.current_amount * card.worth_rate
-
-            card.current_amount = 0.
-            card.save()
-        else:
-            card.current_amount-= rest_amount
-
-            worth += rest_amount * card.worth_rate
-            card.save()
-            break
-
-    # TODO: exception if rest_amount is not 0.
-
-    return worth
--- shareflixweb/shareflix/models.py
+++ shareflixweb/shareflix/models.py
@@ -5,28 +5,16 @@ from django.conf import settings
 from django.utils.translation import gettext_lazy as _
 
 
-class Currency(models.TextChoices):
-    EUR = 'EUR', _('Euro')
-    TL = 'TL', _('Lyra')
-
-
 class NetflixAccount(models.Model):
     def __str__(self):
         return self.email
 
-    monthly_costs = models.DecimalField(
-        max_digits=10, decimal_places=2, null=True,)
-    currency = models.CharField(
-        max_length=3,
-        choices=Currency.choices,
-        default=Currency.TL)
     email = models.CharField(
         max_length=30)
     password = models.CharField(
         max_length=64)
     pin = models.DecimalField(max_digits=4, decimal_places=0)
     active = models.BooleanField()
-    billing_date = models.DateField()
 
 
 class NetflixGiftCard(models.Model):
@@ -34,13 +22,8 @@ class NetflixGiftCard(models.Model):
         return self.redemption_date
 
     redemption_date = models.DateField()
-    currency = models.CharField(
-        max_length=3,
-        choices=Currency.choices,
-        default=Currency.TL)
-    original_amount = models.DecimalField(max_digits=10, decimal_places=2, null=True,)
-    current_amount = models.DecimalField(max_digits=10, decimal_places=2, null=True,)
-    worth_rate = models.DecimalField(max_digits=10, decimal_places=2, null=True,) # get the "worth" amount - example Euro. 15TL * 1,5465blabla = 2bla Euro
+    redem_worth_amount = models.DecimalField(max_digits=10, decimal_places=2, null=True,)
+    cost = models.DecimalField(max_digits=10, decimal_places=2, null=True,)
     account = models.ForeignKey(
         NetflixAccount,
         on_delete=models.PROTECT)
